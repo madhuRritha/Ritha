2bbdf493149f461422987e73cd494a4138d09bdf
madhuRritha <30748250+madhuRritha@users.noreply.github.com>
2017-08-05 16:41:39 +0530

    evennos.c

---
+++ evennos.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("enter the a value:");
+scanf(""%d",&a);
+if(a%2==0)
+{
+printf("the no is even");
+}
+else 
+{
+printf("the no is odd");
+}
+getch();
+}
