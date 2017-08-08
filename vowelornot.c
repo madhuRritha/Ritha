cf521a4507cd4979b697b5b10eebfc25f1e2d340
madhuRritha <30748250+madhuRritha@users.noreply.github.com>
2017-08-08 16:33:12 +0530

    vowelornot
    .c

---
+++ vowelornot.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+char vowel;
+printf("enterthe alpabet:");
+scanf("%s",vowels);
+if(vowels=a,e,i,o,u,A,E,I,O,U)
+{
+printf("the alpabet is vowel");
+else
+printf("the alpabet is consonant");
+}
