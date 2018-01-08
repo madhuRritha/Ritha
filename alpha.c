cef7d940282a19af74d8adfdd30e8967e2879021
madhuRritha <30748250+madhuRritha@users.noreply.github.com>
2018-01-08 16:26:01 +0530

    alpha.c

---
+++ alpha.c
@@ -0,0 +1,10 @@
+void main()
+{
+char c;
+printf("enter the character:");
+scanf("%c",c);
+if((c>='a'&&c<='z')||(c>='A'&&c='Z'))
+printf("it is an alphabet");
+else
+printf("it is not an alphabet");
+}
