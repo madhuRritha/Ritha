edcb48d810f4b6de4f6d57ddae9eb2ede8a8b3de
madhuRritha <30748250+madhuRritha@users.noreply.github.com>
2018-01-10 16:59:32 +0530

    helloworld.c

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int i,n;
+clrscr();
+printf("enter the number:");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+printf("helloworld\n");
+}
+getch();
+}
