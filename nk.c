ca7622bc017c7c61304256a92602eb5214ea274b
madhuRritha <30748250+madhuRritha@users.noreply.github.com>
2018-01-22 16:26:14 +0530

    nk.c

---
+++ nk.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int i,n[5],k,sum=0;
+clrscr();
+printf("enter the array elements are:");
+for(i=0;i<n[i];i++)
+scanf("%d",&i);
+printf("enter the k value:");
+scanf("%d",&k);
+if(k<0)
+{
+sum=sum+n[i];
+i++;
+}
+printf("the output is:",&sum);
+getch();
+return 0;
+}
