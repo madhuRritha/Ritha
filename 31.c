e45428dd10449425032650957437918e10cb180e
madhuRritha <30748250+madhuRritha@users.noreply.github.com>
2018-02-07 16:47:28 +0530

    Update 31.c

--- 31.c
+++ 31.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
+#include<conio.h>
+#include<string.h>
 int main()
 {
 char s;
 printf("enter the string");
 scanf("%s",s);
 printf("length is %d",strlen(s));
+  getch();
 return 0;
 }
