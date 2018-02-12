decd1a661b500245b9d38110447d0c83242a728f
madhuRritha <30748250+madhuRritha@users.noreply.github.com>
2018-02-12 16:34:13 +0530

    49.c

---
+++ 49.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int a,b,c;
+clrscr();
+printf("enter the  2 numbers:");
+scanf("%d %d",&a,&b);
+c=a+b;
+printf("the output is:%d\n",c);
+getch();
+return 0;
+}
