d5417b9315aea9455f8a5e6d5a68815c35f8b8c1
madhuRritha <30748250+madhuRritha@users.noreply.github.com>
2018-02-12 16:56:30 +0530

    46.c

---
+++ 46.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n,sum;
+clrscr();
+printf("enter the number:");
+scanf("%d",&n);
+sum=n+1;
+printf("the output is:%d",sum);
+getch();
+return 0;
+}
