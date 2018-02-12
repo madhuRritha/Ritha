45fc910bed82fe87017119f5238e49cb87ae9b70
madhuRritha <30748250+madhuRritha@users.noreply.github.com>
2018-02-12 16:15:51 +0530

    43.c

---
+++ 43.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char m[1000],n[1000];
+clrscr();
+printf("enter the string 1 and string 2:");
+scanf("%s %s",m,n);
+printf("%s%s,m,n);
+getch();
+return 0;
+}
