afa4eb78bb0170307ec04df61e4d2a768bfc00a6
madhuRritha <30748250+madhuRritha@users.noreply.github.com>
2018-02-12 16:27:44 +0530

    44.c

---
+++ 44.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int n;
+clrscr();
+printf("enter the number:");
+scanf("%d",&n);
+if(n<=10)
+{
+printf("yes");
+}
+else
+printf("no");
+getch();
+return 0;
+}
