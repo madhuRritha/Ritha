a55e5b409dd0cc50502a3874af6bc2303c3afbfc
madhuRritha <30748250+madhuRritha@users.noreply.github.com>
2018-02-23 16:24:12 +0530

    65.c

---
+++ 65.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char m;
+clrscr();
+printf("enter string:");
+scanf("%s\t",m);
+if(m!= ' ')
+{
+if(printf("%s",m))
+{
+}
+}
+getch();
+return 0;
+}
