4649dac05f6f9948980c223bbfd627598ba26c62
madhuRritha <30748250+madhuRritha@users.noreply.github.com>
2018-03-07 16:47:40 +0530

    82.c

---
+++ 82.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+float l,b,area;
+clrscr();
+printf("enter the length:");
+scanf("%f",&l);
+printf("enter the breadth:");
+scanf("%f",&b);
+area=l*b;
+printf("the area is:%f",area);
+getch();
+}
