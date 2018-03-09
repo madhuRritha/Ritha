eae787ca4f5b67164706716e6b666f26b69b632f
madhuRritha <30748250+madhuRritha@users.noreply.github.com>
2018-03-09 16:01:36 +0530

    81.c

---
+++ 81.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+int i,n,m,l;
+printf("\n enter the two numbers:\n");
+for(i=0;i<3;i++)
+{
+scanf("%d  %d",&n,&m);
+l=m-n;
+printf("\n %d",l);
+}
+getch();
+return 0;
+}
