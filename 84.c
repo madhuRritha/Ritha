bc66af91482e406f93e321f2024155e92a599092
madhuRritha <30748250+madhuRritha@users.noreply.github.com>
2018-03-09 16:09:23 +0530

    84.c

---
+++ 84.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+char ch;
+int a;
+clrscr();
+printf("\n enter the character:\n");
+scanf("%c",&ch);
+a=(int)ch;
+printf("%d",a);
+getch();
+return 0;
+}
