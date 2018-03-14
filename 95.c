ed789f8ceff35b992e26caacf36bc1eb7b55e78f
madhuRritha <30748250+madhuRritha@users.noreply.github.com>
2018-03-14 16:58:34 +0530

    95.c

---
+++ 95.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int p,n,r,k;
+clrscr();
+printf("enter the p:");
+scanf("%d",&p);
+printf("enter the n:");
+scanf("%d",&n);
+printf("enter the r:");
+scanf("%d",&r);
+k=(p*n*r)/100;
+printf("the simple intrest value is:%d",k);
+getch();
+}
