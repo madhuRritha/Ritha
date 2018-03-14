2b6d9c40a565f300e56e100e2dea219560882972
madhuRritha <30748250+madhuRritha@users.noreply.github.com>
2018-03-14 16:19:42 +0530

    Update 92.c

--- 92.c
+++ 92.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 #include<conio.h>
-int main() 
+int main()
 {
 int n,i,sum=0;
 clrscr();
 printf("\n enter the N number:\n");
 scanf("%d",&n);
 printf("enter the numbers:");
-for(i=0;i<n;i++)
+for(i=1;i<=n;i++)
 {
-scanf("%d",i);
-sum=i++;
+scanf("%d",&i);
+sum=sum+i;
 }
 printf("%d",sum);
 getch();
