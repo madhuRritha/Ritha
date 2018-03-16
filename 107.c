6d1387fd8713fc521e6e51674b19aef743127675
madhuRritha <30748250+madhuRritha@users.noreply.github.com>
2018-03-16 16:44:12 +0530

    107.c

---
+++ 107.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+int a,b,c,o;
+clrscr();
+printf("enter the a:");
+scanf("%d",&a);
+printf("enter the b:");
+scanf("%d",&b);
+printf("enter the c:");
+scanf("%d",&c);
+o=(a*b)/c;
+printf("the output:%d",o);
+getch();
+return 0;
+}
