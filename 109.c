73483ea251e3125300ec24103414a84966dbf8a5
madhuRritha <30748250+madhuRritha@users.noreply.github.com>
2018-03-19 16:57:44 +0530

    109.c

--- 109.c
+++ 109.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
-int main(void) {
-	int a[10],i,j,t;
-	for(i=1;i<=10;i++)
-	{
-		scanf("%d",&a[i]);
-	}
-	for(i=1;i<=10;i++)
-	{
-		for(j=i+1;j<=10;j++)
-		{
-			if(a[i]>a[j])
-			{
-				t=a[i];
-				a[i]=a[j];
-				a[j]=t;
-			}
-		}
-	}
-	printf("%d",a[1]);
-	return 0;
+#include<conio.h>
+int main() 
+{
+int a[10],i,j,t;
+clrscr();
+printf("enter the numbers:");
+for(i=1;i<=10;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=1;i<=10;i++)
+{
+for(j=i+1;j<=10;j++)
+{
+if(a[i]>a[j])
+{
+t=a[i];
+a[i]=a[j];
+a[j]=t;
+}
+}
+}
+printf("the minimum number is:%d",a[1]);
+getch();
+return 0;
 }
