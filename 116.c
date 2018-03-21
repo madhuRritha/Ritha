21b12e67e30f1a7d91a000bafe4c0031c8d9847e
madhuRritha <30748250+madhuRritha@users.noreply.github.com>
2018-03-21 16:24:12 +0530

    116.c

---
+++ 116.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+int n,k;
+clrscr();
+printf("enter the value of N:");
+scanf("%d",&n);
+printf("enter the value of K:");
+scanf("%d",&k);
+printf("the joint number is:%d%d\n",n,k);
+getch();
+return 0;
+}
